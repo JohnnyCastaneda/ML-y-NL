b5901d5e796a38e7eea0b2fed4a712bcabf81ad5
Johnny Castañeda <pjohnny@uninorte.edu.co>
2023-04-29 17:43:11 -0500

    Update m09_model_deployment.py

--- m09_model_deployment.py
+++ m09_model_deployment.py
@@ -5,7 +5,7 @@
 
 
 # Importación librerías
-get_ipython().run_line_magic('matplotlib', 'inline')
+#get_ipython().run_line_magic('matplotlib', 'inline')
 import pandas as pd
 import numpy as np
 from random import randrange
